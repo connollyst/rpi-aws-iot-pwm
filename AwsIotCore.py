32ddb4033288cbabbe7f9ff4da8a2dc4a9f91bb8
Sean Connolly <connolly.st@gmail.com>
2021-09-06 13:50:53 -1000

    Added AWS IoT dependencies (not yet integrated)

---
+++ AwsIotCore.py
@@ -0,0 +1,67 @@
+#!/usr/bin/python
+
+import time
+
+from awscrt import io, mqtt
+from awsiot import mqtt_connection_builder
+
+
+class AwsIotCore:
+
+    def __init__(self,
+                 endpoint,
+                 cert_filepath='certs/device.pem.crt',
+                 ca_filepath='certs/Amazon-root-CA-1.pem',
+                 private_key_filepath='certs/private.pem.key'):
+        self._endpoint = endpoint
+        self._cert_filepath = cert_filepath
+        self._ca_filepath = ca_filepath
+        self._private_key_filepath = private_key_filepath
+        self._connection = None
+
+    @property
+    def endpoint(self):
+        return self._endpoint
+
+    @property
+    def cert_filepath(self):
+        return self._cert_filepath
+
+    @property
+    def ca_filepath(self):
+        return self._ca_filepath
+
+    @property
+    def private_key_filepath(self):
+        return self._private_key_filepath
+
+    def connect(self, client_id):
+        event_loop_group = io.EventLoopGroup(1)
+        host_resolver = io.DefaultHostResolver(event_loop_group)
+        client_bootstrap = io.ClientBootstrap(event_loop_group, host_resolver)
+        mqtt_connection = mqtt_connection_builder.mtls_from_path(
+            endpoint=self.endpoint,
+            client_bootstrap=client_bootstrap,
+            cert_filepath=self.cert_filepath,
+            pri_key_filepath=self.private_key_filepath,
+            ca_filepath=self.ca_filepath,
+            client_id=client_id,
+            clean_session=False,
+            keep_alive_secs=30,
+        )
+        print("Connecting to {} with client ID '{}'...".format(self.endpoint, client_id))
+        connect_future = mqtt_connection.connect()
+        connect_future.result()
+        self._connection = mqtt_connection
+        print("Connected!")
+
+    def write(self, topic, json):
+        print("Sending to {}: {}".format(topic, json))
+        self._connection.publish(topic=topic, payload=json, qos=mqtt.QoS.AT_LEAST_ONCE)
+        time.sleep(1)
+
+    def disconnect(self):
+        print("Disconnecting from AWS...")
+        disconnect_future = self._connection.disconnect()
+        disconnect_future.result()
+        print("Disconnected from AWS.")
--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM balenalib/raspberry-pi-debian-python:latest
+FROM balenalib/raspberry-pi-debian:latest
 
 MAINTAINER Sean Connolly <connolly.st@gmail.com>
 
@@ -14,10 +14,19 @@ RUN apt-get update && apt-get install -y \
     --no-install-recommends && \
     rm -rf /var/lib/apt/lists/*
 
+RUN sudo apt-get update && \
+    sudo apt-get install -y \
+    cmake \
+    python3-dev \
+    python3-pip \
+    --no-install-recommends && \
+    rm -rf /var/lib/apt/lists/*
+
 COPY requirements.txt /
-RUN pip install -r requirements.txt
+RUN python3 -m pip install -r requirements.txt
 
 COPY main.py /
 COPY PCA9685.py /
-
-CMD [ "python", "./main.py" ]
+COPY AwsIotCore.py /
+COPY certs/ /certs/
+CMD [ "python3", "./main.py" ]
