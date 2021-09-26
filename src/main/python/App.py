e1ad829d661aac3ab83e0d9b3367c31aa662d52a
Sean Connolly <connolly.st@gmail.com>
2021-09-25 19:44:52 -1000

    Moved JSON serialization to MotorDriver

--- src/main/python/App.py
+++ src/main/python/App.py
@@ -29,28 +29,12 @@ class App:
         print('Running {} @ {} for {}s'.format(self._direction, self._speed, self._duration))
         writer = AwsIotCore(self.AWS_ENDPOINT)
         writer.connect(self.AWS_CLIENT_ID)
-        message = {
-            "address": "N/A",
-            "addressType": "N/A",
-            "name": "",
-            "module": "Motor Driver",
-            "version": "0.1",
-            "reading": {}
-        }
         motor = MotorDriver()
-        message['reading'] = {
-            "value": self._speed,
-            "timestamp": time.time()
-        }
-        writer.write(self.AWS_IOT_MQTT_TOPIC, json.dumps(message, indent=4, default=str))
+        writer.write(self.AWS_IOT_MQTT_TOPIC, json.dumps(motor.to_json(), indent=4, default=str))
         motor.run(0, self._direction, self._speed)
         motor.run(1, self._direction, self._speed)
         time.sleep(self._duration)
         print("Stopping.")
         motor.stop(0)
         motor.stop(1)
-        message['reading'] = {
-            "value": 0,
-            "timestamp": time.time()
-        }
-        writer.write(self.AWS_IOT_MQTT_TOPIC, json.dumps(message, indent=4, default=str))
+        writer.write(self.AWS_IOT_MQTT_TOPIC, json.dumps(motor.to_json(), indent=4, default=str))
--- src/main/python/pwm/MotorDriver.py
+++ src/main/python/pwm/MotorDriver.py
@@ -1,3 +1,4 @@
+import time
 from enum import Enum
 
 from PCA9685 import PCA9685
@@ -18,6 +19,7 @@ class MotorDriver:
     BIN2 = 4
 
     def __init__(self):
+        self._speed = 0  # There are multiple motors!
         self._pwm = PCA9685(0x40, debug=False)
         self._pwm.setPWMFreq(self.PWM_FREQUENCY)
 
@@ -32,18 +34,38 @@ class MotorDriver:
             raise Exception('Motor parameter out of bounds, expected 0 or 1 but received {}', motor)
 
     def _run(self, pwm, in1, in2, direction, speed):
-        self._pwm.setDutycycle(pwm, speed)
         if direction == MotorDriver.Direction.FORWARD:
+            self._pwm.setDutycycle(pwm, speed)
             self._pwm.setLevel(in1, 0)
             self._pwm.setLevel(in2, 1)
+            self._speed = speed
         elif direction == MotorDriver.Direction.REVERSE:
+            self._pwm.setDutycycle(pwm, speed)
             self._pwm.setLevel(in1, 1)
             self._pwm.setLevel(in2, 0)
+            self._speed = speed
         else:
             raise Exception('Unrecognized direction parameter: {}', direction.name)
 
     def stop(self, motor):
         if motor == 0:
             self._pwm.setDutycycle(self.PWMA, 0)
-        else:
+            self._speed = 0
+        elif motor == 1:
             self._pwm.setDutycycle(self.PWMB, 0)
+            self._speed = 0
+        else:
+            raise Exception('Motor parameter out of bounds, expected 0 or 1 but received {}', motor)
+
+    def to_json(self):
+        return {
+            "address": "N/A",
+            "addressType": "N/A",
+            "name": "",
+            "module": "Motor Driver",
+            "version": "0.2",
+            "reading": {
+                "value": self._speed,
+                "timestamp": time.time()
+            }
+        }
