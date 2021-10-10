29d51a4054ea8c4eb6d0e1fded4d9bcf26bf014a
Sean Connolly <connolly.st@gmail.com>
2021-10-10 13:28:21 -1000

    Initial commit of CodeBuild config

--- src/test/python/app/test_App.py
+++ src/test/python/app/test_App.py
@@ -14,3 +14,13 @@ class test_App(unittest.TestCase):
         App(speed, duration, frequency)
         # Then
         # self.assertEqual(0, motor_driver.to_json()['reading']['value'])
+
+    def test_should_run(self):
+        # Given
+        speed = 20
+        duration = 30
+        frequency = 60
+        app = App(speed, duration, frequency)
+        # When
+        # TODO how to test long-running process?
+        # app.start()
